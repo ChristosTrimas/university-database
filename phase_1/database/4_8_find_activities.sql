e1d03915cc343ffcc215be3c32ad9be405b6da26
Christos Trimas <christos.trimas@gmail.com>
2020-05-19 20:37:07 +0300

    Update 4_8_find_activities.sql

--- phase_1/database/4_8_find_activities.sql
+++ phase_1/database/4_8_find_activities.sql
@@ -1,4 +1,9 @@
-CREATE OR REPLACE FUNCTION public.find_4_8_room_dif_sub()
+-- FUNCTION: public.find_4_8_room_dif_sub()
+
+--DROP FUNCTION public.find_4_8_room_dif_sub();
+
+CREATE OR REPLACE FUNCTION public.find_4_8_room_dif_sub(
+	)
     RETURNS TABLE(summary integer, room integer) 
     LANGUAGE 'plpgsql'
 
@@ -9,7 +14,7 @@ AS $BODY$
 BEGIN
 RETURN query
 
-select cast(count(la.course_code) as integer), r.room_id
+select  cast(count(la.course_code) as integer)as summary,r.room_id
 from "LearningActivity" la, "Room" r, "Semester" se
 where la.room_id = r.room_id and se.semester_status = 'present' and la.serial_number = se.semester_id
 group by r.room_id;
